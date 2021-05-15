9699a017c3043c250e7f3fd385729d45726d3f80
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-15 16:09:34 -0300

    Finished

---
+++ index.js
@@ -0,0 +1,23 @@
+// Incrementando de 1 a 10
+for (let index = 1; index <= 10; index++) {
+   console.log(index);
+}
+
+
+// Decrementando de 10 a 0
+let counter = 10;
+
+while(counter >= 1){
+    console.log(counter);
+    counter--;
+}
+
+// Números ímpares no intervalo
+for(let i = 1; i <= 100; i += 2){
+    console.log(i);
+}
+
+// Números pares no intervalo
+for(let i = 0; i <= 100; i += 2){
+    console.log(i);
+}
